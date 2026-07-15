79955c892c8d48c86df038da8638eed633fdb0bb
Lewbb <leticiawbaby@gmail.com>
2026-07-15 17:24:10 -0300

    corrige origem do cors github pages

--- takegirls-backend/src/server.js
+++ takegirls-backend/src/server.js
@@ -17,7 +17,7 @@ app.use(cors({
     'http://localhost:5500',
     'http://127.0.0.1:5500',
     /\.vercel\.app$/,
-    'https://lewbb.github.io/take-girls/'
+    'https://lewbb.github.io'
   ],
   credentials: true,
 }));
